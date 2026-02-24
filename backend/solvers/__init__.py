535203053476350de4cb9afbb4f1b91bbf273c85
creativity2472001-ctrl <creativity2472001@gmail.com>
2026-02-25 00:06:30 +0200

    Update __init__.py

--- backend/solvers/__init__.py
+++ backend/solvers/__init__.py
@@ -1,11 +1,5 @@
 """
-ملف الربط بين جميع الحلول
+ملف الربط بين الحلول
 """
 from . import arithmetic
 from . import equations
-from . import derivatives
-from . import integrals
-from . import limits
-from . import simplify
-from . import factor
-from . import normalizer
