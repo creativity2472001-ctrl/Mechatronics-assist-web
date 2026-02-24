838db273ec656239a2a01e58357a3449f854470b
creativity2472001-ctrl <creativity2472001@gmail.com>
2026-02-24 03:44:44 +0200

    Create __init__.py

---
+++ backend/solvers/__init__.py
@@ -0,0 +1,8 @@
+# هذا الملف يجعل المجلد package
+from . import arithmetic
+from . import equations
+from . import derivatives
+from . import integrals
+from . import limits
+from . import simplify
+from . import factor
